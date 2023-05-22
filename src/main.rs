d37d6b5e51d37df7deeb9b049de91469fc69bf30
daves <saurek115@gmail.com>
2023-05-22 11:37:02 -0500

    add terminal output

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,23 @@ mod lib {
     pub(crate) mod request_handler;
 }
 
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
 #[tokio::main]
 async fn main() {
 
+    //cool rainbow effect on launch text
+    let colors = [31, 33, 32, 36, 34, 35, 37]; // ANSI color codes
+    let mut out = String::new();
+
+    for (i, letter) in "JollyFolder".chars().enumerate() {
+        let color = colors[i % colors.len()];
+        out.push_str(&format!("\x1b[{}m{}\x1b[0m", color, letter));
+    }
+
+    println!("Starting up {out} {VERSION}");
+
+    //start the actual application
     if let Err(e) = init().await {
         eprintln!("The program has crashed: {:?}", e)
     }
@@ -22,7 +36,8 @@ async fn main() {
 }
 
 pub struct AppState {
-    pub(crate) root_path: PathBuf
+    pub(crate) root_path: PathBuf,
+    pub(crate) port: u16
 }
 
 async fn init() -> Result<(), AppErrorInternal> {
@@ -30,11 +45,27 @@ async fn init() -> Result<(), AppErrorInternal> {
     let app_state = {
         let args = parse_cli_args::parse_args()?;
         Arc::new(AppState {
-            root_path: args.path
+            root_path: args.path,
+            port: 30080
         })
     };
 
-    println!("Serving directory: {}", app_state.root_path.display());
+    println!("Serving path: {}", app_state.root_path.display());
+    println!("Bound to [::]:{}, 0.0.0.0:{}", app_state.port, app_state.port);
+    let ifaces = get_if_addrs::get_if_addrs().unwrap();
+
+    println!("Available at (non-exhaustive list):");
+
+    for iface in ifaces {
+        match iface.addr {
+            get_if_addrs::IfAddr::V4(addr) => {
+                println!("    http://{}:{}", addr.ip, app_state.port);
+            }
+            get_if_addrs::IfAddr::V6(addr) => {
+                println!("    http://[{}]:{}", addr.ip, app_state.port);
+            }
+        }
+    }
 
     let app = axum::Router::new()
         .route("/", get(request_handler::handle_root_path))
