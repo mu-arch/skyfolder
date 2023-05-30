6a5158a881182733db5034bb447f09cab06c9153
daves <saurek115@gmail.com>
2023-05-29 22:15:47 -0500

    complete search backend

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,68 +1,12 @@
-let GLOBAL_table_data;
+'use strict';
+
+let GLOBAL_TABLE_DATA;
 
 function navurl(url) {
     window.location.href = url;
 }
 
-/*
-function levenshtein(a, b) {
-    let t = [], u, i, j, m = a.length, n = b.length;
-    if (!m) return n;
-    if (!n) return m;
-    for (j = 0; j <= n; j++) t[j] = j;
-    for (i = 1; i <= m; i++) {
-        for (u = [i], j = 1; j <= n; j++) {
-            u[j] = a[i-1] === b[j-1] ? t[j-1] : Math.min(t[j-1], t[j], u[j-1]) + 1;
-        }
-        t = u;
-    }
-    return t[n];
-}
- */
-
-function damerauLevenshtein(a, b) {
-    let matrix = [];
-    let aLen = a.length;
-    let bLen = b.length;
-
-    for (let i = 0; i <= aLen; i++) {
-        matrix[i] = [];
-        matrix[i][0] = i;
-    }
-
-    for (let j = 0; j <= bLen; j++) {
-        matrix[0][j] = j;
-    }
-
-    for (let i = 1; i <= aLen; i++) {
-        for (let j = 1; j <= bLen; j++) {
-            let cost = a[i - 1] === b[j - 1] ? 0 : 1;
-
-            matrix[i][j] = Math.min(
-                matrix[i - 1][j] + 1, // deletion
-                matrix[i][j - 1] + 1, // insertion
-                matrix[i - 1][j - 1] + cost // substitution
-            );
-
-            if (i > 1 && j > 1 && a[i - 1] === b[j - 2] && a[i - 2] === b[j - 1]) {
-                matrix[i][j] = Math.min(
-                    matrix[i][j],
-                    matrix[i - 2][j - 2] + cost // transposition
-                );
-            }
-        }
-    }
-
-    return matrix[aLen][bLen];
-}
-
-function fuzzySearch(list, query, n) {
-    let results = list.map((item) => ({ item, distance: damerauLevenshtein(item, query) }));
-    results.sort((a, b) => a.distance - b.distance);
-    return results.slice(0, n);
-}
-
-function checkIfTbodyIsEmpty() {
+function check_if_tbody_is_empty() {
     const table = document.querySelector('#table');
     const tbody = table.querySelector('tbody');
 
@@ -70,57 +14,82 @@ function checkIfTbodyIsEmpty() {
         table.querySelector('thead tr:last-child').style = 'border-bottom: 1px solid #e5e5e5';
     }
 }
+function extractTableData() {
+    const tbody = document.querySelector('tbody');  // get the tbody element
+    const rows = tbody.querySelectorAll('tr'); // get all 'tr' elements within the tbody
 
-function populateTable(tableBody, data) {
-    // clear table
-    while (tableBody.firstChild) {
-        tableBody.removeChild(tableBody.firstChild);
-    }
-
-    // populate table with data
-    data.forEach((row) => {
-        let tr = document.createElement('tr');
-        let tdName = document.createElement('td');
-        let tdModified = document.createElement('td');
-        let tdSize = document.createElement('td');
-
-        let aName = document.createElement('a');
-        aName.href = row.item.name;
-        aName.textContent = row.item.name;
-        tdName.appendChild(aName);
-
-        tdModified.textContent = row.item.modified;
-        tdSize.textContent = row.item.size;
-
-        tr.appendChild(tdName);
-        tr.appendChild(tdModified);
-        tr.appendChild(tdSize);
-
-        tableBody.appendChild(tr);
+    // Map each 'tr' to an array and return the result
+    return Array.from(rows).map((row, index) => {
+        const aTag = row.querySelector('a');  // get the 'a' tag within the current 'tr'
+        return [aTag.innerText.toLowerCase(), index]; // return the array for the current 'tr', and we lowercase the inner text ready to search
     });
 }
 
-
-function convertTableToObject(tableBody) {
-    let data = [];
-    let rows = tableBody.rows;
-    for (let i = 0; i < rows.length; i++) {
-        let row = rows[i];
-        let cols = row.cells;
-        let rowData = {
-            name: cols[0].getElementsByTagName('a')[0].textContent,
-            modified: cols[1].getAttribute('aria-modified'),
-            size: cols[2].getAttribute('aria-size'),
-        };
-        data.push(rowData);
+function fuzzysearch(needle, haystack) {
+    var hlen = haystack.length;
+    var nlen = needle.length;
+    if (nlen > hlen) {
+        return false;
     }
-    return data;
+    if (nlen === hlen) {
+        return needle === haystack;
+    }
+    outer: for (var i = 0, j = 0; i < nlen; i++) {
+        var nch = needle.charCodeAt(i);
+        while (j < hlen) {
+            if (haystack.charCodeAt(j++) === nch) {
+                continue outer;
+            }
+        }
+        return false;
+    }
+    return true;
+}
+function marshall_search(query, n, rowMatrix) {
+    const matches = rowMatrix
+        .map((row, index) => ({
+            index: row[1],  // index of the original row
+            match: fuzzysearch(query, row[0])  // does query match the string?
+        }))
+        .filter(m => m.match)  // keep only rows where query matches the string
+
+    // return the indexes of the matching rows
+    return matches.slice(0, n).map(m => m.index);
 }
 
-function searchAndPopulateTable(searchQuery, resultsLimit) {
-    let searchResults = fuzzySearch(GLOBAL_table_data, searchQuery, resultsLimit);
-    populateTable(tableBody, searchResults);
+
+function displaySearchResults(indexes) {
+    const tbody = document.getElementById('b');  // get the original tbody
+    tbody.style.display = 'none';  // set the original tbody to display none
+
+    // if b2 already exists, delete it
+    const oldB2 = document.getElementById('b2');
+    if (oldB2) {
+        oldB2.remove();
+    }
+
+    // create a new tbody with id="b2"
+    const b2 = document.createElement('tbody');
+    b2.id = 'b2';
+
+    // for each index, copy the corresponding row from the original tbody to b2
+    for (const index of indexes) {
+        const row = tbody.rows[index];
+        const clonedRow = row.cloneNode(true);  // clone the row
+        b2.appendChild(clonedRow);  // append the cloned row to b2
+    }
+
+    // insert b2 after the original tbody
+    tbody.parentNode.insertBefore(b2, tbody.nextSibling);
 }
+
+
+function search(query, rows) {
+    let results = marshall_search(query, rows, GLOBAL_TABLE_DATA);
+    displaySearchResults(results)
+}
+
+
 
 
 //onmouseover="preloadNextPage(this)"
@@ -139,7 +108,8 @@ function searchAndPopulateTable(searchQuery, resultsLimit) {
 */
 
 function first_run_manifest() {
-    checkIfTbodyIsEmpty()
+    check_if_tbody_is_empty()
+    GLOBAL_TABLE_DATA = extractTableData();
 }
 
 document.addEventListener('DOMContentLoaded', first_run_manifest);
